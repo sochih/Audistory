eeaa3debcb431b64a691066f2821feadeeafba33
Radian <craziestdavid@gmail.com>
2018-06-18 17:11:13 +0800

    m

---
+++ Assets/Scripts/ASPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using AudiStory;
+
+namespace AudiStory {
+
+    public class ASPlayer : MonoBehaviour
+    {
+        public static ASPlayer Instance; 
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
+
+
